0150d624e1f47a2383a035e7594bd26a8a5e4b83
Mark Baggett <mbagget1@utk.edu>
2020-10-27 13:59:37 -0400

    Clean up excess printing.

--- src/generate_pdfs.nim
+++ src/generate_pdfs.nim
@@ -1,16 +1,21 @@
-import osproc, strformat, os, strutils
+import osproc, strformat, os, strutils, nimoji, random
+
+proc get_a_book_emoji(): string =
+  let 
+    books = [":book:", ":notebook:", ":blue_book:", ":orange_book:", ":green_book:", ":closed_book:"]
+  sample(books)
 
 proc get_directory(directory: string): seq[string] =
   for kind, path in walkDir(directory):
     if kind == pcDir:
       result.add(path)
-      echo path
 
 proc generate_pdf(directories: seq[string], quality, output_directory: string): int =
-  echo directories
   for directory in directories:
-    let last = directory.split("/")[^1]
-    let my_command = fmt"convert {directory}/*.tif -compress jpeg -quality {quality} {output_directory}/{last}.pdf"
+    let
+      last = directory.split("/")[^1]
+      my_command = fmt"convert {directory}/*.tif -compress jpeg -quality {quality} {output_directory}/{last}.pdf"
+    echo fmt"{'\n'}Generating PDF for {last}. {get_a_book_emoji()} {'\n'}".emojize
     discard execShellCmd(my_command)
 
 when isMainModule:
